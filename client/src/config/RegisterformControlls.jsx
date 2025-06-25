3db36969392e64d881d3859384cf46aa8cb75da4
Aswabnth.v <aswanthv695@gmail.com>
2025-06-25 09:16:46 -0700

    creating login ui

---
+++ client/src/config/RegisterformControlls.jsx
@@ -0,0 +1,25 @@
+import { Type } from "lucide-react";
+
+export const RegisterformControlls =[
+    {
+        name:'userName',
+        label:'User Name',
+        placeholder:'Enter Username',
+        type:'text',
+        componentType:'input'
+    },
+    {
+        name:'email',
+        label:'User email',
+        placeholder:'Enter Email',
+        type:'email',
+        componentType:'input'
+    },
+    {
+        name:'password',
+        label:'User password',
+        placeholder:'Enter Password',
+        type:'password',
+        componentType:'input'
+    }
+]
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../index.css';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
+  console.log("register");
+  
   return (
-    <div>Register</div>
+    <div className='mx-auto w-full max-w-md space-y-6'>
+      <div className='text-center'>
+       <h1 className="text-muted-foreground font-bold text-3xl tracking-tight">sign-up</h1>
+     <p>
+  Already have an account?{" "}
+  <Link
+    className="font-medium text-primary hover:underline transition-transform cursor-pointer"
+    to="/auth/login"
+  >
+    Login
+  </Link>
+</p>
+
+      </div>
+    </div>
   )
 }
 
