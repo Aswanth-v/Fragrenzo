900cf19453dd67222006ad2414abee7552d1a555
Aswabnth.v <aswanthv695@gmail.com>
2025-07-17 01:36:51 -0700

    added curved

--- client/src/pages/shopping/Home.jsx
+++ client/src/pages/shopping/Home.jsx
@@ -5,6 +5,7 @@ import Banner3 from "../../assets/Banner3.png";
 import { Button } from "../../components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Curved from '../../components/Curved.jsx'
 const Home = () => {
   const settings = {
     infinite: true,
@@ -35,25 +36,36 @@ const Home = () => {
           ))}
         </Slider>
       </div>
+<div className="max-h-96">
 
-     
-      <span className="flex justify-center pt-6 text-xl sm:text-2xl font-bold">
+     <Curved
+marqueeText="Fragenzo ✦ For ✦ Him ✦ For ✦ Her ✦ For ✦ You ✦"
+
+  speed={3}
+  curveAmount={-200}
+  direction="right"
+  interactive={true}
+ className="fill-black"
+/>
+</div>
+      <span className="flex justify-center  text-xl sm:text-2xl font-bold">
         Categories
       </span>
 
-      <div className="w-full flex flex-wrap justify-center gap-6 md:gap-12 lg:gap-20 py-8">
-        {["Men", "Women", "Unisex"].map((label) => (
-          <Link
-            key={label}
-            to={"/shop/list"}
-            className="no-underline"
-          >
-            <Button className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 bg-white text-black border border-gray-300 hover:bg-gray-100 shadow-md rounded-full text-base sm:text-lg font-semibold flex items-center justify-center">
-              {label}
-            </Button>
-          </Link>
-        ))}
-      </div>
+     <div className="w-full flex flex-wrap justify-center gap-2 sm:gap-8 md:gap-4 lg:gap-6 py-6">
+  {["Men", "Women", "Unisex"].map((label) => (
+    <Link
+      key={label}
+      to={"/shop/list"}
+      className="no-underline"
+    >
+      <Button className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 bg-white text-black border border-gray-300 hover:bg-gray-100 shadow-md rounded-full text-base sm:text-lg font-semibold flex items-center justify-center">
+        {label}
+      </Button>
+    </Link>
+  ))}
+</div>
+
     </div>
   );
 };
